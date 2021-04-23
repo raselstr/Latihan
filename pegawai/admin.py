2bc9ccc78160216cd1e36d0eb6552ebea8695161
raselstr <str.rasel29@gmail.com>
2021-04-24 02:22:46 +0700

    mengelola Table di Database dengan Model

--- pegawai/admin.py
+++ pegawai/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from pegawai.models import pegawai, pangkat
 # Register your models here.
+
+class PegawaiAdmin(admin.ModelAdmin):
+    list_display = ['nip','nama','jabatan','pangkat']
+    search_fields = ['nama','jabatan']
+    list_filter = ('pangkat',)
+    list_per_page = 5
+
+admin.site.register(pegawai, PegawaiAdmin)
+admin.site.register(pangkat)
+
--- pegawai/models.py
+++ pegawai/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class pangkat(models.Model):
+    gol = models.CharField(max_length=6)
+    pangkat = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.gol
+
+
 class pegawai(models.Model):
     nip = models.CharField(max_length=18)
     nama = models.CharField(max_length=25)
     jabatan = models.CharField(max_length=50)
-    
+    pangkat = models.ForeignKey(pangkat, on_delete=models.CASCADE, null=True)
+
+
 
     def __str__(self):
         return self.nip
