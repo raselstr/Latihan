d52ad928bf43584726b0eaa6eafcca0fa1eddd4a
raselstr <str.rasel29@gmail.com>
2021-04-24 01:51:05 +0700

    tes ulang pust membuat tabel

--- bpkad/urls.py
+++ bpkad/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pegawai.views import pegawai
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pegawai/', pegawai)
 ]
--- pegawai/views.py
+++ pegawai/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def pegawai(request):
+    nama = ["budi", "anto", "iwan"]
+    alamat = "air joman"
+
+    konteks = {
+        'nama':nama,
+        'alamat':alamat,
+    }
+    return render(request, 'pegawai.html', konteks)
