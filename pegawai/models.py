359cd73302907596bc89ae5b05ed7052949f2196
raselstr <str.rasel29@gmail.com>
2021-04-24 01:50:12 +0700

    setelah membuat table di mysql

--- pegawai/models.py
+++ pegawai/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class pegawai(models.Model):
+    nip = models.CharField(max_length=18)
+    nama = models.CharField(max_length=25)
+    jabatan = models.CharField(max_length=50)
+    
+
+    def __str__(self):
+        return self.nip
