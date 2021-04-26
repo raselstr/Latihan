3111b051d85d01dc3bbcaef01823cec0529883fe
raselstr <str.rasel29@gmail.com>
2021-04-26 12:06:27 +0700

    pindah PC

--- bpkad/settings.py
+++ bpkad/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'kantor',
         'USER': 'root',
-        'PASSWORD': 'r283l8tr',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
